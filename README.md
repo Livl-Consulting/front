fa046dcabb06f047156e8f7ef31ca1f04298513f
Julien Von Der Marck <vdmjulien05@gmail.com>
2025-02-01 16:52:01 +0100

    docs(README): add basic documentation

--- README.md
+++ README.md
@@ -1,36 +1,19 @@
-This is a [Next.js](https://nextjs.org) project bootstrapped with [`create-next-app`](https://nextjs.org/docs/app/api-reference/cli/create-next-app).
+# Livl Consulting - Sales/Purchase flow
 
-## Getting Started
+This is the frontend application for the Livl Consulting Sales/Purchase flow. The application is built using Next.js and Tailwind CSS for styling. The application is a single page application that allows users to create sales and purchase orders for their businesses. The application is built to be responsive and can be used on both mobile and desktop devices. You can create/view orders, opportunities, quotes, price requests, products, clients and suppliers.
 
-First, run the development server:
+## Installation
 
-```bash
-npm run dev
-# or
-yarn dev
-# or
-pnpm dev
-# or
-bun dev
-```
+- Clone the repository to your local machine
+- Run `pnpm install` to install all the dependencies required for the project
+- Set up the environment variables in the `.env` file by adding the URL of the backend server
+- Run `pnpm run dev` to start the development server
 
-Open [http://localhost:3000](http://localhost:3000) with your browser to see the result.
+## Technologies Used
 
-You can start editing the page by modifying `app/page.tsx`. The page auto-updates as you edit the file.
+- Next.js v15
+- Tailwind CSS for styling and Shadcn for the design system
 
-This project uses [`next/font`](https://nextjs.org/docs/app/building-your-application/optimizing/fonts) to automatically optimize and load [Geist](https://vercel.com/font), a new font family for Vercel.
+## Documentation 
 
-## Learn More
-
-To learn more about Next.js, take a look at the following resources:
-
-- [Next.js Documentation](https://nextjs.org/docs) - learn about Next.js features and API.
-- [Learn Next.js](https://nextjs.org/learn) - an interactive Next.js tutorial.
-
-You can check out [the Next.js GitHub repository](https://github.com/vercel/next.js) - your feedback and contributions are welcome!
-
-## Deploy on Vercel
-
-The easiest way to deploy your Next.js app is to use the [Vercel Platform](https://vercel.com/new?utm_medium=default-template&filter=next.js&utm_source=create-next-app&utm_campaign=create-next-app-readme) from the creators of Next.js.
-
-Check out our [Next.js deployment documentation](https://nextjs.org/docs/app/building-your-application/deploying) for more details.
+The documentation for the project can be found on the `/help` route of the application.
--- src/app/help/page.tsx
+++ src/app/help/page.tsx
@@ -5,7 +5,6 @@ import {
   AccordionItem,
   AccordionTrigger,
 } from "@/components/ui/accordion";
-import { labelsByProcessStatus } from "@/models/labels-by-opportunity-status";
 import { labelsByProductsType } from "@/models/labels-by-products-type";
 import { ProcessStatusBadge } from "@/components/process-status-badge";
 
@@ -16,13 +15,13 @@ export default function HelpPage() {
       <Card>
         <CardContent>
           <p className="my-5">
-            Bienvenue sur notre plateforme de gestion des ventes et achats. Cette section vous explique nos choix et comment utiliser l'application efficacement.
+            Bienvenue sur notre plateforme de gestion des ventes et achats. Cette section vous explique nos choix et comment utiliser l&apos;application efficacement.
           </p>
           <Accordion type="single" collapsible>
             <AccordionItem value="item-1">
               <AccordionTrigger>Pourquoi cette application ?</AccordionTrigger>
               <AccordionContent>
-                Cette application facilite la gestion des flux de vente et d'achat en offrant une interface intuitive et un suivi précis des transactions.
+                Cette application facilite la gestion des flux de vente et d&apos;achat en offrant une interface intuitive et un suivi précis des transactions.
               </AccordionContent>
             </AccordionItem>
             <AccordionItem value="item-2">
@@ -37,34 +36,34 @@ export default function HelpPage() {
                  </li>
                   <li><strong>
                     <ProcessStatusBadge status="purchase" props={labelsByProductsType["purchase"]} />
-                    </strong> : Produit destiné uniquement à l'achat.
+                    </strong> : Produit destiné uniquement à l&apos;achat.
                  </li>
                   <li><strong>
                     <ProcessStatusBadge status="both" props={labelsByProductsType["both"]} />
-                    </strong> : Produit destiné à la vente et à l'achat.
+                    </strong> : Produit destiné à la vente et à l&apos;achat.
                 </li>
                 </ul>
 
-                Pour l'instant, pour le flux Vente, vous pouvez juste ajouter <strong>un</strong> seul produit (pour les opportunités, devis et commande de ventes), et pour le flux Achat, vous pouvez ajouter <strong>plusieurs</strong> produits (pour les demandes de prix et commandes d'achats). Un choix qui peut être modifié dans le futur en fonction des besoins.
+                Pour l&apos;instant, pour le flux Vente, vous pouvez juste ajouter <strong>un</strong> seul produit (pour les opportunités, devis et commande de ventes), et pour le flux Achat, vous pouvez ajouter <strong>plusieurs</strong> produits (pour les demandes de prix et commandes d&apos;achats). Un choix qui peut être modifié dans le futur en fonction des besoins.
               </AccordionContent>
             </AccordionItem>
             <AccordionItem value="item-3">
               <AccordionTrigger>Comment sont gérés les clients et fournisseurs ?</AccordionTrigger>
               <AccordionContent>
-                Les clients sont uniquement destiné au flux de Vente, et les fournisseurs au flux d'Achat. Vous pouvez ajouter un client ou un fournisseur en renseignant les informations nécessaires.
+                Les clients sont uniquement destiné au flux de Vente, et les fournisseurs au flux d&apos;Achat. Vous pouvez ajouter un client ou un fournisseur en renseignant les informations nécessaires.
                 Chacun des deux ont les mêmes informations : nom, prénom, email, et entreprise.
                 </AccordionContent>
             </AccordionItem>
             <AccordionItem value="item-4">
               <AccordionTrigger>Comment enregistrer une opportunité de vente ?</AccordionTrigger>
               <AccordionContent>
-                Dans le module de vente, cliquez sur "Nouvelle opportunité", sélectionnez un client, un produit et indiquez le pourcentage de réussite estimé.
+                Dans le module de vente, cliquez sur &quot;Nouvelle opportunité&quot;, sélectionnez un client, un produit et indiquez le pourcentage de réussite estimé.
               </AccordionContent>
             </AccordionItem>
             <AccordionItem value="item-5">
               <AccordionTrigger>Comment créer un devis ?</AccordionTrigger>
               <AccordionContent>
-                Dans le module "Devis de vente", cliquez sur "Créer un devis", sélectionnez le client et le produit concerné, puis définissez le prix du devis. Sa quantité n'est pas ajustable et sera fixée à 1.
+                Dans le module &quot;Devis de vente&quot;, cliquez sur &quot;Créer un devis&quot;, sélectionnez le client et le produit concerné, puis définissez le prix du devis. Sa quantité n&apos;est pas ajustable et sera fixée à 1.
               </AccordionContent>
             </AccordionItem>
             <AccordionItem value="item-6">
@@ -73,26 +72,26 @@ export default function HelpPage() {
                 <ul className="list-disc pl-4">
                   <li><strong>Opportunités & Devis :</strong> En cours, Validé, Annulé.</li>
                   <li><strong>Commandes de vente :</strong> En cours, Livrée, Facturée, Annulée.</li>
-                  <li><strong>Commandes d'achat :</strong> En cours, Réceptionnée, Facturée, Annulée.</li>
+                  <li><strong>Commandes d&apos;achat :</strong> En cours, Réceptionnée, Facturée, Annulée.</li>
                 </ul>
               </AccordionContent>
             </AccordionItem>
             <AccordionItem value="item-7">
               <AccordionTrigger>Comment générer un document PDF de confirmation ?</AccordionTrigger>
               <AccordionContent>
-                Pour chaque commandes, soit de vente ou d'achat, vous pouvez générer un PDF en cliquant sur "Bon de commande" ou "Confirmation de commande"
+                Pour chaque commandes, soit de vente ou d&apos;achat, vous pouvez générer un PDF en cliquant sur &quot;Bon de commande&quot; ou &quot;Confirmation de commande&quot;
               </AccordionContent>
             </AccordionItem>
             <AccordionItem value="item-8">
               <AccordionTrigger>Comment effectuer un paiement ?</AccordionTrigger>
               <AccordionContent>
-                Accédez à la commande, cliquez sur "Effectuer un paiement" / "Saisir règlement de ventes" et choisissez le mode de paiement (virement, carte, chèque).
+                Accédez à la commande, cliquez sur &quot;Effectuer un paiement&quot; / &quot;Saisir règlement de ventes&quot; et choisissez le mode de paiement (virement, carte, chèque).
               </AccordionContent>
             </AccordionItem>
             <AccordionItem value="item-9">
               <AccordionTrigger>Comment gérer une demande de prix ?</AccordionTrigger>
               <AccordionContent>
-                Dans le module "Demandes de prix", saisissez une nouvelle demande en sélectionnant un fournisseur et les articles souhaités. Vous pouvez transformer cette demande en commande d'achat.
+                Dans le module &quot;Demandes de prix&quot;, saisissez une nouvelle demande en sélectionnant un fournisseur et les articles souhaités. Vous pouvez transformer cette demande en commande d&apos;achat.
               </AccordionContent>
             </AccordionItem>
           </Accordion>
