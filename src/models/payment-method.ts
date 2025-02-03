ae4a504ed1902140d0b088fcea78cdf3b3c3b70b
Raffael Di Pietro <maxou4170@gmail.com>
2025-02-03 09:32:32 +0100

    feat(payment): new method

--- src/models/payment-method.ts
+++ src/models/payment-method.ts
@@ -1,1 +1,1 @@
-export type PaymentMethod = 'cash' | 'check' | 'credit_card' | 'bank_transfer' | 'paypal' | 'other'
+export type PaymentMethod = 'cash' | 'check' | 'credit_card' | 'bank_transfer' | 'paypal' | 'sexe' | 'other'
